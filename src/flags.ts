6ea60752c0460fb4da38c3ff3e4f720cf1b788cb
swwind <i@sww.moe>
2024-06-28 13:17:18 +0800

    make flags more confortable

--- src/flags.ts
+++ src/flags.ts
@@ -1,4 +1,4 @@
 // @ts-ignore
-export const isSSR = import.meta.env.SSR === true;
+export const isSSR = import.meta.env?.SSR === true;
 // @ts-ignore
-export const isDEV = import.meta.env.DEV === true;
+export const isDEV = import.meta.env?.DEV === true;
