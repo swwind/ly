b445dbe2a2471ee236ef7fb69281905aa041e4f2
swwind <i@sww.moe>
2024-07-06 10:35:20 +0800

    upgrade exbuild

--- examples/firefly/src/routes/index.tsx
+++ examples/firefly/src/routes/index.tsx
@@ -1,5 +1,17 @@
-import { component$ } from "@swwind/ly";
+import { component$, effect, ref } from "@swwind/ly";
 
 export default component$(() => {
-  return <div>hello world</div>;
+  const count = ref(0);
+  const increment = () => count.value++;
+  effect(() => console.log(`count = ${count.value}`));
+
+  return (
+    <div>
+      <h1>hello world2</h1>
+      <p>
+        <button onClick={increment}>+1</button>
+      </p>
+      <p>count = {count}</p>
+    </div>
+  );
 });
